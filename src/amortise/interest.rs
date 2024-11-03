b8dd56b352a9f7a7832c04a43d3fae0cc64d486e
David Neale <neale.dj@gmail.com>
2024-11-03 21:57:28 +0000

    Interest calc tests

--- src/amortise/interest.rs
+++ src/amortise/interest.rs
@@ -77,3 +77,123 @@ pub fn calculate_period_interest(
         Some(INTEREST_ROUNDING),
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn test_get_daily_interest_rate() {
+        let annual_rate = dec!(0.05); // 5% annual interest rate
+
+        assert_eq!(
+            get_daily_interest_rate(annual_rate, InterestMethod::Convention30_360),
+            dec!(0.0001388888888888888888888889)
+        );
+        assert_eq!(
+            get_daily_interest_rate(annual_rate, InterestMethod::Actual365),
+            dec!(0.0001369863013698630136986301)
+        );
+        assert_eq!(
+            get_daily_interest_rate(annual_rate, InterestMethod::Actual360),
+            dec!(0.0001388888888888888888888889)
+        );
+        assert_eq!(
+            get_daily_interest_rate(annual_rate, InterestMethod::ActualActual),
+            dec!(0.0001369863013698630136986301)
+        );
+    }
+
+    #[test]
+    fn test_calculate_period_interest_convention30_360() {
+        let start_date = NaiveDate::from_ymd_opt(2023, 1, 1).expect("Invalid date");
+        let to_date = NaiveDate::from_ymd_opt(2023, 1, 30).expect("Invalid date");
+        let payment_date = NaiveDate::from_ymd_opt(2023, 1, 15).expect("Invalid date");
+        let daily_rate = dec!(0.0001388888888888888888888889);
+        let balance = dec!(1000);
+        let payment_amount = dec!(100);
+        let interest_method = InterestMethod::Convention30_360;
+
+        let interest = calculate_period_interest(
+            start_date,
+            to_date,
+            payment_date,
+            daily_rate,
+            balance,
+            payment_amount,
+            interest_method,
+        );
+
+        assert_eq!(interest, dec!(4.1666666666666666666666670000));
+    }
+
+    #[test]
+    fn test_calculate_period_interest_actual365() {
+        let start_date = NaiveDate::from_ymd_opt(2023, 1, 1).expect("Invalid date");
+        let to_date = NaiveDate::from_ymd_opt(2023, 1, 30).expect("Invalid date");
+        let payment_date = NaiveDate::from_ymd_opt(2023, 1, 15).expect("Invalid date");
+        let daily_rate = dec!(0.0001369863013698630136986301);
+        let balance = dec!(1000);
+        let payment_amount = dec!(100);
+        let interest_method = InterestMethod::Actual365;
+
+        let interest = calculate_period_interest(
+            start_date,
+            to_date,
+            payment_date,
+            daily_rate,
+            balance,
+            payment_amount,
+            interest_method,
+        );
+
+        assert_eq!(interest, dec!(3.89));
+    }
+
+    #[test]
+    fn test_calculate_period_interest_actual360() {
+        let start_date = NaiveDate::from_ymd_opt(2023, 1, 1).expect("Invalid date");
+        let to_date = NaiveDate::from_ymd_opt(2023, 1, 30).expect("Invalid date");
+        let payment_date = NaiveDate::from_ymd_opt(2023, 1, 15).expect("Invalid date");
+        let daily_rate = dec!(0.0001388888888888888888888889);
+        let balance = dec!(1000);
+        let payment_amount = dec!(100);
+        let interest_method = InterestMethod::Actual360;
+
+        let interest = calculate_period_interest(
+            start_date,
+            to_date,
+            payment_date,
+            daily_rate,
+            balance,
+            payment_amount,
+            interest_method,
+        );
+
+        assert_eq!(interest, dec!(3.94));
+    }
+
+    #[test]
+    fn test_calculate_period_interest_actualactual() {
+        let start_date = NaiveDate::from_ymd_opt(2023, 1, 1).expect("Invalid date");
+        let to_date = NaiveDate::from_ymd_opt(2023, 1, 30).expect("Invalid date");
+        let payment_date = NaiveDate::from_ymd_opt(2023, 1, 15).expect("Invalid date");
+        let daily_rate = dec!(0.0001369863013698630136986301);
+        let balance = dec!(1000);
+        let payment_amount = dec!(100);
+        let interest_method = InterestMethod::ActualActual;
+
+        let interest = calculate_period_interest(
+            start_date,
+            to_date,
+            payment_date,
+            daily_rate,
+            balance,
+            payment_amount,
+            interest_method,
+        );
+
+        assert_eq!(interest, dec!(3.89));
+    }
+}
